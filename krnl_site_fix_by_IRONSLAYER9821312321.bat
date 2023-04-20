b04a8da8b597c78a863833b615afb96e5d17422c
sanae <96895806+sanaeOS@users.noreply.github.com>
2023-04-20 07:48:37 -0400

    Add files via upload

---
+++ krnl_site_fix_by_IRONSLAYER9821312321.bat
@@ -0,0 +1,22 @@
+@echo off
+title optimizer genshin
+color 06
+
+@echo [+] flushing dns
+ipconfig /flushdns
+
+@echo [+] deleting logs
+cd/
+del *.log /a /s /q /f
+
+@echo [+] deleting cache
+
+RD /S /Q %temp%
+MKDIR %temp%
+takeown /f "%temp%" /r /d y
+takeown /f "C:\Windows\Temp" /r /d y
+RD /S /Q C:\Windows\Temp
+MKDIR C:\Windows\Temp
+takeown /f "C:\Windows\Temp" /r /d y
+takeown /f %temp% /r /d y
+pause
